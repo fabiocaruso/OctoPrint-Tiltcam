13992a3f8c757378677d1a37ff9864de4b588fee
Fabio Caruso <fabio.caruso@hotmail.com>
2020-01-28 16:32:51 +0000

    Skeleton

---
+++ octoprint_TiltCam/static/js/TiltCam.js
@@ -0,0 +1,29 @@
+/*
+ * View model for OctoPrint-Tiltcam
+ *
+ * Author: Fabio Caruso
+ * License: MIT
+ */
+$(function() {
+    function TiltcamViewModel(parameters) {
+        var self = this;
+
+        // assign the injected parameters, e.g.:
+        // self.loginStateViewModel = parameters[0];
+        // self.settingsViewModel = parameters[1];
+
+        // TODO: Implement your plugin's view model here.
+    }
+
+    /* view model class, parameters for constructor, container to bind to
+     * Please see http://docs.octoprint.org/en/master/plugins/viewmodels.html#registering-custom-viewmodels for more details
+     * and a full list of the available options.
+     */
+    OCTOPRINT_VIEWMODELS.push({
+        construct: TiltcamViewModel,
+        // ViewModels your plugin depends on, e.g. loginStateViewModel, settingsViewModel, ...
+        dependencies: [ /* "loginStateViewModel", "settingsViewModel" */ ],
+        // Elements to bind to, e.g. #settings_plugin_TiltCam, #tab_plugin_TiltCam, ...
+        elements: [ /* ... */ ]
+    });
+});
